7e861bfeffd2562f0fa45c4c4e9a2e71bf70f88a
Phạm Trần Thuỷ Vy <vy.ptt.64cntt@ntu.edu.vn>
2025-02-26 22:23:49 +0700

    tính tổng

--- Ex_3/app/src/main/java/vy/ex_3_simplesumapp/MainActivity.java
+++ Ex_3/app/src/main/java/vy/ex_3_simplesumapp/MainActivity.java
@@ -1,6 +1,8 @@
 package vy.ex_3_simplesumapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void XulyCong (View view){
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKQ = findViewById(R.id.edtKQ);
+
+        String strA = editTextSoA.getText().toString();
+        String strB = editTextSoB.getText().toString();
+        //chuyer du lieu sang dang so
+
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+
+        //tinh tong
+
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);
+
+        //hien ra man hinh
+
+        editTextKQ.setText(strTong);
+
+
+
     }
 }
