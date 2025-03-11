523c0915e66ebad490ba5c88beada54937ef0831
Phạm Trần Thuỷ Vy <vy.ptt.64cntt@ntu.edu.vn>
2025-03-11 08:03:06 +0700

    cập nhật lại màn hình

--- Ex7_IntentLogin/app/src/main/java/vy/ex7_intentlogin/LoginActivity.java
+++ Ex7_IntentLogin/app/src/main/java/vy/ex7_intentlogin/LoginActivity.java
@@ -1,6 +1,11 @@
 package vy.ex7_intentlogin;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +20,24 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        EditText edtUsername = findViewById(R.id.edtUsername);
+        EditText edtPassword = findViewById(R.id.edtPassword);
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edtUsername.getText().toString();
+                String password = edtPassword.getText().toString();
+
+                if (username.equals("admin") && password.equals("123456")) {
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Sai tài khoản hoặc mật khẩu!", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
