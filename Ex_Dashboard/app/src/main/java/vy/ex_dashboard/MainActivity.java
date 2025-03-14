0cf918b8ffc18d73c88a1a47df3ba8b6810c4194
Phạm Trần Thuỷ Vy <vy.ptt.64cntt@ntu.edu.vn>
2025-03-14 16:57:15 +0700

    đang bị lỗi

---
+++ Ex_Dashboard/app/src/main/java/vy/ex_dashboard/MainActivity.java
@@ -0,0 +1,31 @@
+package vy.ex_dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import vy.ex_dashboard.Activity_Clothing;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView clothingCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        clothingCard = findViewById(R.id.clothingCard);
+
+        clothingCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Activity_Clothing.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
