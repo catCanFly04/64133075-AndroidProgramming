ba54acba1df46b7c61605f67fec0d0f0908ce468
Phạm Trần Thuỷ Vy <vy.ptt.64cntt@ntu.edu.vn>
2025-03-21 15:28:44 +0700

    đang làm layout homeactivity

--- Ex_KidsCorner2/app/src/main/java/vy/ex_kidscorner/MainActivity.java
+++ Ex_KidsCorner2/app/src/main/java/vy/ex_kidscorner/MainActivity.java
@@ -1,6 +1,8 @@
 package vy.ex_kidscorner;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish(); // Đóng MainActivity
+            }
+        }, 3000);
     }
 }
