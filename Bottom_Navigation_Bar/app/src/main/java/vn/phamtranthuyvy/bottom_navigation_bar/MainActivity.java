9a9ad183e4163255411f561a29f0ab55192151b3
Phạm Trần Thuỷ Vy <vy.ptt.64cntt@ntu.edu.vn>
2025-04-23 22:20:25 +0700

    layout

---
+++ Bottom_Navigation_Bar/app/src/main/java/vn/phamtranthuyvy/bottom_navigation_bar/MainActivity.java
@@ -0,0 +1,31 @@
+package vn.phamtranthuyvy.bottom_navigation_bar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNav;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        bottomNav = findViewById(R.id.bot_nav);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
+
+    }
+}
--- thigk2PhamTranThuyVy/app/src/main/java/thigk2/phamtranthuyvy/Cau4Activity.java
+++ thigk2PhamTranThuyVy/app/src/main/java/thigk2/phamtranthuyvy/Cau4Activity.java
@@ -10,11 +10,5 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Cau4Activity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_cau4);
 
     }
-}
